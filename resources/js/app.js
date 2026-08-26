3ccfa1864029a94492cef75925e36b8be160e147
Jefferson Tadeu dos Santos <tadeu.santos7148@gmail.com>
2026-08-25 23:10:59 -0300

    chore(ui): add bootstrap icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,8 @@
-import '../css/app.css';
-
 import { createInertiaApp } from '@inertiajs/vue3';
 import { createApp, h } from 'vue';
 
 const pages = import.meta.glob('./pages/**/*.vue', { eager: true });
-const applicationName = import.meta.env.VITE_APP_NAME || 'Aplicação';
+const applicationName = import.meta.env.VITE_APP_NAME || 'Transcrev';
 
 createInertiaApp({
     title: (title) => (title ? `${title} — ${applicationName}` : applicationName),
